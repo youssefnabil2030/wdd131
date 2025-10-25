2a8af17c8e2dba8d146a3bb86c6a632753dae26d
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2025-10-25 15:01:39 -0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Dynamic Copyright Year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentYear").textContent = currentYear;
-
-// Dynamic Last Modified Date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = lastModified;
