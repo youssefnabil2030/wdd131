82cffe99b5877a099a784c660efe327f96666542
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2025-10-25 15:00:18 -0700

    Add script to display current year and last modified date

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Dynamic Copyright Year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+// Dynamic Last Modified Date
+document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
