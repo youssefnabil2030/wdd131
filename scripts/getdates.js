de98c26b9eb99dfc6f4163bf640729855708c772
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2025-10-27 16:38:05 -0700

    Delete scripts directory

--- scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-// Dynamic Copyright Year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentYear").textContent = currentYear;
-
-// Dynamic Last Modified Date
-document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
